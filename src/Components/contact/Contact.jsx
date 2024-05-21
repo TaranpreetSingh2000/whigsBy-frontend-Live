aa8686e1266c2522eb761b0c1cf811c0bb25331a
TaranpreetSingh2000 <taranpreets598@gmail.com>
2024-05-21 11:36:02 +0530

    Update Navbar

--- src/Components/contact/Contact.jsx
+++ src/Components/contact/Contact.jsx
@@ -1,68 +1,64 @@
 import React, { useState } from "react";
 
 const Contact = () => {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [subject, setSubject] = useState("");
+  const [message, setMessage] = useState("");
 
-  const [name, setName]= useState("")
-  const [email, setEmail]= useState("")
-  const [subject, setSubject]= useState("")
-  const [message, setMessage]= useState("")
-
-  
   return (
-    <section class="bg-white dark:bg-gray-900">
-      <div class="py-8 lg:py-16 px-4 mx-auto max-w-screen-lg">
-        <h2 class="mb-4 text-4xl tracking-tight font-extrabold text-center text-gray-900 dark:text-white">
+    <section className="bg-white">
+      <div className="py-8 lg:py-16 px-4 mx-auto max-w-screen-lg">
+        <h2 className="mb-4 text-4xl tracking-tight font-extrabold text-center">
           Contact Us
         </h2>
-        <p class="mb-8 lg:mb-16 font-light text-center text-gray-500 dark:text-gray-400 sm:text-xl">
+        <p className="mb-8 lg:mb-16 font-light text-center text-gray-500 dark:text-gray-400 sm:text-xl">
           Got a technical issue? If you have any inquiries about the site or our
           products, please feel free to send us on email. Let us know.
         </p>
-        <form  class="space-y-8" action="https://api.web3forms.com/submit" method="POST">
-        <input type="hidden" name="access_key" value="a29fffd1-ae53-4a8f-8ef3-15f42c727384"/>
- 
-        <div>
-            <label
-              for="text"
-              class="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300"
-            >
-            Name
+        <form
+          className="space-y-8"
+          action="https://api.web3forms.com/submit"
+          method="POST"
+        >
+          <input
+            type="hidden"
+            name="access_key"
+            value="a29fffd1-ae53-4a8f-8ef3-15f42c727384"
+          />
+
+          <div>
+            <label for="text" className="block mb-2 text-sm font-medium">
+              Name
             </label>
             <input
               type="text"
               id="name"
               name="name"
               value={name}
-              onChange={(e)=> setName(e.target.value)}
-              class="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500 dark:shadow-sm-light"
+              onChange={(e) => setName(e.target.value)}
+              className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 "
               placeholder="Enter your name"
               required
             />
           </div>
           <div>
-            <label
-              for="email"
-              class="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300"
-            >
-              Your email
+            <label for="email" className="block mb-2 text-sm font-medium">
+              Email
             </label>
             <input
               type="email"
               id="email"
               name="email"
               value={email}
-              onChange={(e)=> setEmail(e.target.value)}
-
-              class="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500 dark:shadow-sm-light"
+              onChange={(e) => setEmail(e.target.value)}
+              className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 "
               placeholder="name@flowbite.com"
               required
             />
           </div>
           <div>
-            <label
-              for="subject"
-              class="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300"
-            >
+            <label for="subject" className="block mb-2 text-sm font-medium ">
               Subject
             </label>
             <input
@@ -70,18 +66,14 @@ const Contact = () => {
               id="subject"
               name="subject"
               value={subject}
-              onChange={(e)=> setSubject(e.target.value)}
-
-              class="block p-3 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 shadow-sm focus:ring-primary-500 focus:border-primary-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500 dark:shadow-sm-light"
+              onChange={(e) => setSubject(e.target.value)}
+              className="block p-3 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 shadow-sm focus:ring-primary-500 focus:border-primary-500 "
               placeholder="Let us know how we can help you"
               required
             />
           </div>
-          <div class="sm:col-span-2">
-            <label
-              for="message"
-              class="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-400"
-            >
+          <div className="sm:col-span-2">
+            <label for="message" className="block mb-2 text-sm font-medium">
               Your message
             </label>
             <textarea
@@ -89,15 +81,14 @@ const Contact = () => {
               rows="6"
               name="message"
               value={message}
-              onChange={(e)=> setMessage(e.target.value)}
-
-              class="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg shadow-sm border border-gray-300 focus:ring-primary-500 focus:border-primary-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
+              onChange={(e) => setMessage(e.target.value)}
+              className="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg shadow-sm border border-gray-300 focus:ring-primary-500 focus:border-primary-500 "
               placeholder="Leave a comment..."
             ></textarea>
           </div>
           <button
             type="submit"
-            class="py-3 px-4 text-md font-medium text-center rounded-lg text-white bg-blue-700 focus:ring-4 focus:outline-none hover:bg-blue-600 hover:transition all duration-500 ease-in-out"
+            className="py-3 px-4 text-md font-medium text-center rounded-lg text-white bg-blue-700 focus:ring-4 focus:outline-none hover:bg-blue-600 hover:transition all duration-500 ease-in-out"
           >
             Send message
           </button>
--- src/Components/navbar/Navbar.jsx
+++ src/Components/navbar/Navbar.jsx
@@ -135,7 +135,7 @@ const Navbar = () => {
               <li>
                 <Link
                   to="/"
-                  className="block py-2 px-3 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 md:dark:text-blue-500"
+                  className="block py-2 px-3 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0"
                 >
                   Home
                 </Link>
@@ -143,7 +143,7 @@ const Navbar = () => {
               <li>
                 <Link
                   to="about"
-                  className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
+                  className="block py-2 px-3 text-[#1f3167] rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 "
                 >
                   About
                 </Link>
@@ -151,7 +151,7 @@ const Navbar = () => {
               <li>
                 <Link
                   to="/category"
-                  className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
+                  className="block py-2 px-3 text-[#1f3167] rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500"
                 >
                   Categories
                 </Link>
@@ -159,7 +159,7 @@ const Navbar = () => {
               <li>
                 <Link
                   to="/contact"
-                  className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
+                  className="block py-2 px-3 text-[#1f3167] rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 "
                 >
                   Contact
                 </Link>
