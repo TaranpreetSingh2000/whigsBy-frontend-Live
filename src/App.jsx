d6a530f8517603d722cb1760e3ec02a009d04a82
TaranpreetSingh2000 <taranpreets598@gmail.com>
2024-05-21 11:16:47 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,4 @@
-import React, { useState } from "react";
-import Data from "./Components/data/Data";
+import React, { useState, lazy, Suspense } from "react";
 import {
   Route,
   BrowserRouter as Router,
@@ -10,31 +9,54 @@ import {
 import Signup from "./Components/auth/signup/Signup";
 import Login from "./Components/auth/login/Login";
 import Layout from "./Components/layout/Layout";
-import Dashboard from "./Components/ProductsListing/ProductsListing.jsx";
 import Protected from "./Components/services/Protected.jsx";
 import Admin from "./Components/auth/admin/Admin.jsx";
-import Contact from "./Components/contact/Contact.jsx";
 import AdminProtected from "./Components/services/AdminProtected.jsx";
-import AdminDashboard from "./Components/adminDashboard/AdminDashboard.jsx";
-import ProductDetails from "./Components/productDetails/ProductDetails.jsx";
 import { CartContext } from "./_context/CartContext.js";
-import Cart from "./Components/cart/Cart.jsx";
-import Page from "./cartPage/Page.jsx";
-import Wishlist from "./Components/wishlist/Wishlist.jsx";
-import ShopCategoriesDetails from "./Components/productDetails/ShopCategoriesDetails.jsx";
-import ManageProducts from "./Components/adminDashboard/manageProducts/ManageProducts.jsx";
-import Users from "./Components/adminDashboard/Users.jsx";
-import AdminCart from "./Components/adminDashboard/AdminCart.jsx";
-import Checkout from "./Components/checkout/Checkout.jsx";
-import SuccessPopup from "./Components/successPage/SuccessPopup.jsx";
-import Customers from "./Components/adminDashboard/Customers.jsx";
-import Orders from "./Components/adminDashboard/Orders.jsx";
-import AboutUs from "./Components/about/AboutUs.jsx";
-import CategoriesPage from "./Components/category/CategoriesPage.jsx";
 import { ToastContainer } from "react-toastify";
 import Home from "./Components/home/Home.jsx";
-import ProductsListing from "./Components/ProductsListing/ProductsListing.jsx";
-import Profile from "./Components/profile/Profile.jsx";
+
+// Lazy load components
+const Dashboard = lazy(() =>
+  import("./Components/ProductsListing/ProductsListing.jsx")
+);
+
+const AboutUs = lazy(() => import("./Components/about/AboutUs.jsx"));
+const Page = lazy(() => import("./cartPage/Page.jsx"));
+const SuccessPopup = lazy(() =>
+  import("./Components/successPage/SuccessPopup.jsx")
+);
+const Contact = lazy(() => import("./Components/contact/Contact.jsx"));
+const ProductDetails = lazy(() =>
+  import("./Components/productDetails/ProductDetails.jsx")
+);
+const CategoriesPage = lazy(() =>
+  import("./Components/category/CategoriesPage.jsx")
+);
+const Cart = lazy(() => import("./Components/cart/Cart.jsx"));
+const Wishlist = lazy(() => import("./Components/wishlist/Wishlist.jsx"));
+
+const ShopCategoriesDetails = lazy(() =>
+  import("./Components/productDetails/ShopCategoriesDetails.jsx")
+);
+const ManageProducts = lazy(() =>
+  import("./Components/adminDashboard/manageProducts/ManageProducts.jsx")
+);
+const Users = lazy(() => import("./Components/adminDashboard/Users.jsx"));
+const AdminCart = lazy(() =>
+  import("./Components/adminDashboard/AdminCart.jsx")
+);
+const Checkout = lazy(() => import("./Components/checkout/Checkout.jsx"));
+const Customers = lazy(() =>
+  import("./Components/adminDashboard/Customers.jsx")
+);
+const Orders = lazy(() => import("./Components/adminDashboard/Orders.jsx"));
+
+const Profile = lazy(() => import("./Components/profile/Profile.jsx"));
+
+const AdminDashboard = lazy(() =>
+  import("./Components/adminDashboard/AdminDashboard.jsx")
+);
 
 const router = createBrowserRouter(
   createRoutesFromElements(
@@ -42,45 +64,161 @@ const router = createBrowserRouter(
       <Route path="" element={<Layout />}>
         <Route path="/" element={<Home />} />
         <Route path="signup" element={<Signup />} />
-        <Route path="profile" element={<Profile />} />
-        <Route path="about" element={<AboutUs />} />
-        <Route path="category" element={<CategoriesPage />} />
+        <Route
+          path="profile"
+          element={
+            <Suspense fallback={<div>Loading...</div>}>
+              <Profile />
+            </Suspense>
+          }
+        />
+        <Route
+          path="about"
+          element={
+            <Suspense fallback={<div>Loading...</div>}>
+              <AboutUs />
+            </Suspense>
+          }
+        />
+        <Route
+          path="category"
+          element={
+            <Suspense fallback={<div>Loading...</div>}>
+              <CategoriesPage />
+            </Suspense>
+          }
+        />
         <Route path="login" element={<Login />} />
         <Route path="admin" element={<Admin />} />
         <Route
           path="listing"
-          element={<Protected Component={ProductsListing} />}
+          element={
+            <Suspense fallback={<div>Loading...</div>}>
+              <Protected Component={Dashboard} />
+            </Suspense>
+          }
         />
-
-        <Route path="productDetails/:productId" element={<ProductDetails />} />
+        <Route
+          path="productDetails/:productId"
+          element={
+            <Suspense fallback={<div>Loading...</div>}>
+              <ProductDetails />
+            </Suspense>
+          }
+        />
         <Route
           path="/categoryDetails/:categoryname"
-          element={<Protected Component={ShopCategoriesDetails} />}
+          element={
+            <Suspense fallback={<div>Loading...</div>}>
+              <Protected Component={ShopCategoriesDetails} />
+            </Suspense>
+          }
         />
         <Route
           path="/categoryDetails/:productId"
-          element={<Protected Component={ProductDetails} />}
+          element={
+            <Suspense fallback={<div>Loading...</div>}>
+              <Protected Component={ProductDetails} />
+            </Suspense>
+          }
         />
-
-        <Route path="/cart" element={<Cart />} />
-        <Route path="/wishlist" element={<Protected Component={Wishlist} />} />
-        <Route path="checkout" element={<Protected Component={Checkout} />} />
-        <Route path="/page" element={<Protected Component={Page} />} />
+        <Route
+          path="/cart"
+          element={
+            <Suspense fallback={<div>Loading...</div>}>
+              <Cart />
+            </Suspense>
+          }
+        />
+        <Route
+          path="/wishlist"
+          element={
+            <Suspense fallback={<div>Loading...</div>}>
+              <Protected Component={Wishlist} />
+            </Suspense>
+          }
+        />
+        <Route
+          path="checkout"
+          element={
+            <Suspense fallback={<div>Loading...</div>}>
+              <Protected Component={Checkout} />
+            </Suspense>
+          }
+        />
+        <Route
+          path="/page"
+          element={
+            <Suspense fallback={<div>Loading...</div>}>
+              <Protected Component={Page} />
+            </Suspense>
+          }
+        />
         <Route
           path="/checkout/success"
-          element={<Protected Component={SuccessPopup} />}
+          element={
+            <Suspense fallback={<div>Loading...</div>}>
+              <Protected Component={SuccessPopup} />
+            </Suspense>
+          }
         />
         <Route
           path="admindashboard"
-          element={<AdminProtected Component={AdminDashboard} />}
+          element={
+            <Suspense fallback={<div>Loading...</div>}>
+              <AdminProtected Component={AdminDashboard} />
+            </Suspense>
+          }
         >
-          <Route path="manageproducts" element={<ManageProducts />} />
-          <Route path="user" element={<Users />} />
-          <Route path="admincart" element={<AdminCart />} />
-          <Route path="customer" element={<Customers />} />
-          <Route path="orders" element={<Orders />} />
+          <Route
+            path="manageproducts"
+            element={
+              <Suspense fallback={<div>Loading...</div>}>
+                <ManageProducts />
+              </Suspense>
+            }
+          />
+          <Route
+            path="user"
+            element={
+              <Suspense fallback={<div>Loading...</div>}>
+                <Users />
+              </Suspense>
+            }
+          />
+          <Route
+            path="admincart"
+            element={
+              <Suspense fallback={<div>Loading...</div>}>
+                <AdminCart />
+              </Suspense>
+            }
+          />
+          <Route
+            path="customer"
+            element={
+              <Suspense fallback={<div>Loading...</div>}>
+                <Customers />
+              </Suspense>
+            }
+          />
+          <Route
+            path="orders"
+            element={
+              <Suspense fallback={<div>Loading...</div>}>
+                <Orders />
+              </Suspense>
+            }
+          />
         </Route>
-        <Route path="contact" element={<Contact />} />
+        <Route
+          path="contact"
+          element={
+            <Suspense fallback={<div>Loading...</div>}>
+              <Contact />
+            </Suspense>
+          }
+        />
       </Route>
     </>
   )
